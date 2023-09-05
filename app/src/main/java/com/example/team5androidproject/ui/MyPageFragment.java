bc61cd8aa99d04eeaace3c3c2bd5c9398356d58d
javawebusingapi <maves951@gmail.com>
2023-09-05 17:38:33 +0900

    Fragment를 관리하는 Package추가

--- app/src/main/java/com/example/team5androidproject/ui/MyPageFragment.java
+++ app/src/main/java/com/example/team5androidproject/ui/MyPageFragment.java
@@ -1,4 +1,4 @@
-package com.example.team5androidproject;
+package com.example.team5androidproject.ui;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.team5androidproject.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DetailFragment#newInstance} factory method to
+ * Use the {@link MyPageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DetailFragment extends Fragment {
+public class MyPageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class DetailFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DetailFragment() {
+    public MyPageFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class DetailFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailFragment.
+     * @return A new instance of fragment MyPageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DetailFragment newInstance(String param1, String param2) {
-        DetailFragment fragment = new DetailFragment();
+    public static MyPageFragment newInstance(String param1, String param2) {
+        MyPageFragment fragment = new MyPageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        return inflater.inflate(R.layout.fragment_my_page, container, false);
     }
 }
