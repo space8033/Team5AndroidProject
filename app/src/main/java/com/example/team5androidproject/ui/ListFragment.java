6bc007c912dccc421c50e3aedfe1b5f2e61e8533
javawebusingapi <maves951@gmail.com>
2023-09-06 20:45:58 +0900

    Toolbar(Topbar) 상품목록 추가

--- app/src/main/java/com/example/team5androidproject/ui/ListFragment.java
+++ app/src/main/java/com/example/team5androidproject/ui/ListFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -28,15 +33,32 @@ public class ListFragment extends Fragment {
         navController = NavHostFragment.findNavController(this);
 
         initBtnDetail();
-        initBtnSearch();
+
+        initMenu();
 
         return binding.getRoot();
     }
 
-    private void initBtnSearch() {
-        binding.btnSearch.setOnClickListener(v->{
-            navController.popBackStack();
-        });
+    private void initMenu() {
+        MenuProvider menuProvider = new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.list_fragment,menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.dest_search){
+                    navController.navigate(R.id.action_dest_list_to_dest_search);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.dest_main){
+                    navController.popBackStack(R.id.dest_main,false);
+                    return true;
+                }
+                return false;
+            }
+        };
+        getActivity().addMenuProvider(menuProvider,getViewLifecycleOwner(), Lifecycle.State.RESUMED);
     }
 
     private void initBtnDetail() {
