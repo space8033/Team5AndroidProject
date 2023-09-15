6bd24f6745a70878eaaf9393085bca9e769e09bb
space8033 <space8237@naver.com>
2023-09-15 09:27:57 +0900

    실험

--- app/src/main/java/com/example/team5androidproject/service/NetworkInfo.java
+++ app/src/main/java/com/example/team5androidproject/service/NetworkInfo.java
@@ -2,6 +2,7 @@ package com.example.team5androidproject.service;
 
 public class NetworkInfo {
     public static final String BASE_URL = "http://192.168.0.191:8080/our-twinkling-infinitely/";
+    //이거 왜이래
     //public static final String BASE_URL = "http://192.168.0.20:8080/our-twinkling-infinitely/";
 
 }
